d82d285ccccda64503a4967744ee1964d5259eac
HelloPerseus <perseus.wang2001@gmail.com>
2017-07-24 11:13:09 -0700

    person and hello world

---
+++ Person/src/person/Person.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi im Perseus!");
+        System.out.println("Im 15");
+        System.out.println("And Im from China");
+        System.out.println("I like play League of Legents!");
+        System.out.println("Also white is my favorite color!");
+        String h = "HI!";
+        System.out.println(h);
+    }
+    
+}
